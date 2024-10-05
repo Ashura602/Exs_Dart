d6820dd9a228c0620e5e38b44e2956bfe0e33fc9
VitorSousa.2 <vitorgg1208bug@gmail.com>
2024-10-05 14:31:33 -0300

    commit do vitor repositorio do BG

---
+++ exs_condicionais/vitor/atv.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  String nome = "vitor";
+  if (nome == String){
+    print("programa de testo do vitor fuciona ");
+    print("braga veja logo e não apague");
+
+  }
+
+}
