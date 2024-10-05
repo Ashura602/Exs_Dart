f902a5d7fe1e58ebc139411f253bd35db1becd44
VitorSousa.2 <vitorgg1208bug@gmail.com>
2024-10-05 17:26:32 -0300

    coloquei o teu pai

--- exs_condicionais/vitor/atv.dart
+++ exs_condicionais/vitor/atv.dart
@@ -3,6 +3,8 @@ void main(List<String> args) {
   if (nome != String){
     print("programa de testo do vitor fuciona ");
     print("braga veja logo e não apague");
+
+    
     print('teu paiiiiiii');
 
   }
