d31d9251d2ad20010d57688715b13529e6b2a7df
vitorsousa <vitorgg1208bug@gmail.com>
2024-10-07 18:50:29 -0300

    criei a breanch

--- exs_condicionais/vitor/atv.dart
+++ exs_condicionais/vitor/atv.dart
@@ -3,7 +3,5 @@ void main(List<String> args) {
   if (nome != String) {
     print("programa de testo do vitor fuciona ");
     print("braga veja logo e não apague");
-    print('teu paiiiiiii');
-    print("llçç");
   }
 }
