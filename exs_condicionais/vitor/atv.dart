752fccd7af3d2c49170e89d62f0d38ea44005f9e
vitorsousa <vitorgg1208bug@gmail.com>
2024-10-07 19:11:17 -0300

    adcionei minhas pasta

--- exs_condicionais/vitor/atv.dart
+++ exs_condicionais/vitor/atv.dart
@@ -1,7 +1,6 @@
 void main(List<String> args) {
   int nome = 1;
   if (nome != String) {
-    print("programa de testo do vitor fuciona ");
-    print("braga veja logo e não apague");
+    print("add as pastas");
   }
 }
