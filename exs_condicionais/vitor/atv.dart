2f0cbe9dd7ba6264a08df74ec904ca0524a7cdac
Gab.Braga <gabriel.lima21031@gmail.com>
2024-10-07 18:22:26 -0300

    ajeitei erro no print do vitor

--- exs_condicionais/vitor/atv.dart
+++ exs_condicionais/vitor/atv.dart
@@ -1,11 +1,9 @@
 void main(List<String> args) {
   int nome = 1;
-  if (nome != String){
+  if (nome != String) {
     print("programa de testo do vitor fuciona ");
     print("braga veja logo e não apague");
     print('teu paiiiiiii');
-    print("llçç")
-
+    print("llçç");
   }
-
 }
