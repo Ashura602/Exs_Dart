2a741aeedbbfa6d99e16805b2826578950e5beaa
Gab.Braga <gabriel.lima21031@gmail.com>
2024-10-21 21:37:47 -0300

    Exs.funções 18 e 22 a 25 feitos

--- exs_funcao/Braga/avan/ex20.dart
+++ exs_funcao/Braga/avan/ex20.dart
@@ -1,14 +1,17 @@
-import '../../../util.dart';
+import 'dart:io';
 
-int numFatorial(int number){
-  
-  int total=1;
-  for(i=number; i>1; i--){
+import '../../../util.dart';
+import 'ex19.dart';
+
+int numFatorial(int number) {
+  int total = 1;
+  for (int i = number; i > 1; i--) {
     total *= i;
   }
+  return total;
 }
-// esqeuceu do int no começo do i e num fatorial não existe mas acho que tu confundio com o numfatorial 
+// esqeuceu do int no começo do i e num fatorial não existe mas acho que tu confundio com o numfatorial
 
-main(){
-  numPalavras(int.parse(input('Numero: ')));
+main() {
+  numFatorial(int.parse(input('Numero: ')));
 }
